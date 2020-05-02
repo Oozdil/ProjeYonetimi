8993503fbbad349056a61295aa383340eac1d71c
Oozdil <37013255+Oozdil@users.noreply.github.com>
2020-05-02 18:38:08 +0300

    Proje Yonetimi

---
+++ ProjeYonetimiOrcunOzdil/ProjeYonetimiOrcunOzdil/MainForm.Designer.cs
@@ -0,0 +1,191 @@
+﻿namespace ProjeYonetimiOrcunOzdil
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonTitanic = new System.Windows.Forms.Button();
+            this.buttonRPS = new System.Windows.Forms.Button();
+            this.buttonZombie = new System.Windows.Forms.Button();
+            this.labelForTitanic = new System.Windows.Forms.Label();
+            this.labelForRPS = new System.Windows.Forms.Label();
+            this.labelForZombie = new System.Windows.Forms.Label();
+            this.buttonClose = new System.Windows.Forms.Button();
+            this.panelBorderTop = new System.Windows.Forms.Panel();
+            this.panelBorderFooter = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // buttonTitanic
+            // 
+            this.buttonTitanic.BackColor = System.Drawing.Color.Transparent;
+            this.buttonTitanic.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Black;
+            this.buttonTitanic.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonTitanic.Font = new System.Drawing.Font("Comic Sans MS", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.buttonTitanic.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.buttonTitanic.Location = new System.Drawing.Point(461, 54);
+            this.buttonTitanic.Name = "buttonTitanic";
+            this.buttonTitanic.Size = new System.Drawing.Size(270, 60);
+            this.buttonTitanic.TabIndex = 0;
+            this.buttonTitanic.Text = "Titanic Survival Predictor";
+            this.buttonTitanic.UseVisualStyleBackColor = false;
+            this.buttonTitanic.Click += new System.EventHandler(this.buttonTitanic_Click);
+            // 
+            // buttonRPS
+            // 
+            this.buttonRPS.BackColor = System.Drawing.Color.Transparent;
+            this.buttonRPS.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Black;
+            this.buttonRPS.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonRPS.Font = new System.Drawing.Font("Comic Sans MS", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.buttonRPS.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.buttonRPS.Location = new System.Drawing.Point(461, 146);
+            this.buttonRPS.Name = "buttonRPS";
+            this.buttonRPS.Size = new System.Drawing.Size(270, 60);
+            this.buttonRPS.TabIndex = 1;
+            this.buttonRPS.Text = "Rock Paper Scissors";
+            this.buttonRPS.UseVisualStyleBackColor = false;
+            this.buttonRPS.Click += new System.EventHandler(this.buttonRPS_Click);
+            // 
+            // buttonZombie
+            // 
+            this.buttonZombie.BackColor = System.Drawing.Color.Transparent;
+            this.buttonZombie.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Black;
+            this.buttonZombie.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonZombie.Font = new System.Drawing.Font("Comic Sans MS", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.buttonZombie.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.buttonZombie.Location = new System.Drawing.Point(461, 232);
+            this.buttonZombie.Name = "buttonZombie";
+            this.buttonZombie.Size = new System.Drawing.Size(270, 60);
+            this.buttonZombie.TabIndex = 2;
+            this.buttonZombie.Text = "Zombie Escape";
+            this.buttonZombie.UseVisualStyleBackColor = false;
+            this.buttonZombie.Click += new System.EventHandler(this.buttonZombie_Click);
+            // 
+            // labelForTitanic
+            // 
+            this.labelForTitanic.AutoSize = true;
+            this.labelForTitanic.BackColor = System.Drawing.Color.Transparent;
+            this.labelForTitanic.Font = new System.Drawing.Font("Comic Sans MS", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.labelForTitanic.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.labelForTitanic.Location = new System.Drawing.Point(12, 55);
+            this.labelForTitanic.Name = "labelForTitanic";
+            this.labelForTitanic.Size = new System.Drawing.Size(429, 47);
+            this.labelForTitanic.TabIndex = 3;
+            this.labelForTitanic.Text = "Titanic Survival Predictor";
+            // 
+            // labelForRPS
+            // 
+            this.labelForRPS.AutoSize = true;
+            this.labelForRPS.BackColor = System.Drawing.Color.Transparent;
+            this.labelForRPS.Font = new System.Drawing.Font("Comic Sans MS", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.labelForRPS.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.labelForRPS.Location = new System.Drawing.Point(59, 150);
+            this.labelForRPS.Name = "labelForRPS";
+            this.labelForRPS.Size = new System.Drawing.Size(338, 47);
+            this.labelForRPS.TabIndex = 4;
+            this.labelForRPS.Text = "Rock Paper Scissors";
+            // 
+            // labelForZombie
+            // 
+            this.labelForZombie.AutoSize = true;
+            this.labelForZombie.BackColor = System.Drawing.Color.Transparent;
+            this.labelForZombie.Font = new System.Drawing.Font("Comic Sans MS", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.labelForZombie.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.labelForZombie.Location = new System.Drawing.Point(97, 245);
+            this.labelForZombie.Name = "labelForZombie";
+            this.labelForZombie.Size = new System.Drawing.Size(256, 47);
+            this.labelForZombie.TabIndex = 5;
+            this.labelForZombie.Text = "Zombie Escape";
+            // 
+            // buttonClose
+            // 
+            this.buttonClose.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Black;
+            this.buttonClose.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonClose.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.buttonClose.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.buttonClose.Location = new System.Drawing.Point(723, 11);
+            this.buttonClose.Name = "buttonClose";
+            this.buttonClose.Size = new System.Drawing.Size(30, 30);
+            this.buttonClose.TabIndex = 6;
+            this.buttonClose.Text = "X";
+            this.buttonClose.UseVisualStyleBackColor = true;
+            this.buttonClose.Click += new System.EventHandler(this.buttonClose_Click);
+            // 
+            // panelBorderTop
+            // 
+            this.panelBorderTop.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.panelBorderTop.Location = new System.Drawing.Point(0, 0);
+            this.panelBorderTop.Name = "panelBorderTop";
+            this.panelBorderTop.Size = new System.Drawing.Size(759, 5);
+            this.panelBorderTop.TabIndex = 7;
+            // 
+            // panelBorderFooter
+            // 
+            this.panelBorderFooter.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.panelBorderFooter.Location = new System.Drawing.Point(-1, 314);
+            this.panelBorderFooter.Name = "panelBorderFooter";
+            this.panelBorderFooter.Size = new System.Drawing.Size(759, 5);
+            this.panelBorderFooter.TabIndex = 8;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.BackgroundImage = global::ProjeYonetimiOrcunOzdil.Properties.Resources.GrayBg;
+            this.ClientSize = new System.Drawing.Size(759, 319);
+            this.Controls.Add(this.panelBorderFooter);
+            this.Controls.Add(this.panelBorderTop);
+            this.Controls.Add(this.buttonClose);
+            this.Controls.Add(this.labelForZombie);
+            this.Controls.Add(this.labelForRPS);
+            this.Controls.Add(this.labelForTitanic);
+            this.Controls.Add(this.buttonZombie);
+            this.Controls.Add(this.buttonRPS);
+            this.Controls.Add(this.buttonTitanic);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "MainForm";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonTitanic;
+        private System.Windows.Forms.Button buttonRPS;
+        private System.Windows.Forms.Button buttonZombie;
+        private System.Windows.Forms.Label labelForTitanic;
+        private System.Windows.Forms.Label labelForRPS;
+        private System.Windows.Forms.Label labelForZombie;
+        private System.Windows.Forms.Button buttonClose;
+        private System.Windows.Forms.Panel panelBorderTop;
+        private System.Windows.Forms.Panel panelBorderFooter;
+    }
+}
